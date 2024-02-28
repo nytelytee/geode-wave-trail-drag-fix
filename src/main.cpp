b1665b4a816aa9f62812fe35fd9fdaf8ed5cbd9a
NyteLyte <nytelyte@tuta.io>
2024-02-28 20:11:11 +0100

    fix dual wave trail and previous point bug

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,40 @@
 #include <Geode/modify/PlayerObject.hpp>
 using namespace geode::prelude;
 
-cocos2d::CCPoint prev_position(0, 0);
-bool dont_add_point = true;
-float prev_yVelocity = -3001;
-
 class $modify(PlayerObject) {
-	void update(float p0) {
-		if (m_isDart) {
-			// exiting a straight area, put the point on the previous
-			// frame's position (where the wave was while it was still on
-			// the straight area)
-			if (m_yVelocity != 0 && prev_yVelocity == 0) {
-				if (dont_add_point) dont_add_point = false;
-				else m_waveTrail->addPoint(prev_position);
-				prev_yVelocity = m_yVelocity;
-			}
-			// entering a straight area, put the point on the current
-			// frame's position (the wave just landed on the straight area)
-			else if (m_yVelocity == 0 && prev_yVelocity != 0) {
-				if (dont_add_point) dont_add_point = false;
-				else m_waveTrail->addPoint(m_position);
-				prev_yVelocity = m_yVelocity;
-			}
-			prev_position = m_position;
 
-			// result: straight line on straight area
-		}
+	cocos2d::CCPoint prev_position{0, 0};
+	bool dont_add_point = true;
+	float prev_yVelocity = -3001;
+
+	void update(float p0) {
+		if (!m_isDart) return PlayerObject::update(p0);
+		// exiting a straight area, put the point on the previous
+		// frame's position (where the wave was while it was still on
+		// the straight area)
+		if (m_yVelocity != 0 && m_fields->prev_yVelocity == 0 && !m_fields->dont_add_point)
+			m_waveTrail->addPoint(m_fields->prev_position);
+		// entering a straight area, put the point on the current
+		// frame's position (the wave just landed on the straight area)
+		else if (m_yVelocity == 0 && m_fields->prev_yVelocity != 0 && !m_fields->dont_add_point)
+			m_waveTrail->addPoint(m_position);
+		// when starting a new wave, a point may become stale, leading to
+		// the wave getting connected to a point from the previous wave
+		// this accounts for that
+		else if (m_fields->dont_add_point)
+			m_fields->dont_add_point = false;
+		m_fields->prev_yVelocity = m_yVelocity;
+		m_fields->prev_position = m_position;
+
+		// result: straight line on straight area
 		PlayerObject::update(p0);
 	}
 	void resetStreak() {
-		dont_add_point = true;
+		m_fields->dont_add_point = true;
 		PlayerObject::resetStreak();
+	}
+	void activateStreak() {
+		m_fields->dont_add_point = true;
+		PlayerObject::activateStreak();
 	}
 };
