acc4f077110ed2bea9248b6f5711596be645cb10
NyteLyte <nytelyte@tuta.io>
2024-05-11 14:36:22 +0200

    attempt at better slope handling, doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
 
+
+#define MBO(object, offset, type) (*reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(object) + offset))
+
 using namespace geode::prelude;
 
 class $modify(WTDFPlayerObject, PlayerObject) {
@@ -24,11 +27,13 @@ class $modify(WTDFPlayerObject, PlayerObject) {
     bool justTeleported = false;
     bool teleportedPreviouslySpiderRing = false;
     bool transitionToCollision = false;
-    bool prevWasOnSlope = false;
+    bool wasOnGround = false;
     float portalTargetLine;
     CCPoint previousPos{-12, -12};
     CCPoint currentPos{-12, -12};
     CCPoint nextPosNoCollision{-12, -12};
+
+    GameObject* collidedSlope = nullptr;
   };
 
   void resetObject() {
@@ -43,11 +48,21 @@ class $modify(WTDFPlayerObject, PlayerObject) {
     m_fields->currentPos = getRealPosition();
     PlayerObject::update(deltaTime);
     m_fields->nextPosNoCollision = getRealPosition();
-
-    if (LevelEditorLayer::get() || !m_gameLayer) return;
+    // if the player collides with a slope, this will stop being nullptr
+    m_fields->collidedSlope = nullptr;
+  }
+  
+  bool preSlopeCollision(float p0, GameObject* p1) {
+    bool value = PlayerObject::preSlopeCollision(p0, p1);
+    if (!value) m_fields->collidedSlope = p1;
+    return value;
   }
 
   void postCollision(float deltaTime) {
+
+    bool hitBottom = MBO(this, 0x750, double) != 0;
+    bool hitTop = MBO(this, 0x748, double) != 0;
+    
     PlayerObject::postCollision(deltaTime);
 
     if (LevelEditorLayer::get() || !m_gameLayer) return;
@@ -62,8 +77,9 @@ class $modify(WTDFPlayerObject, PlayerObject) {
     CCPoint nextPositionNoCollision = m_fields->nextPosNoCollision;
     CCPoint nextPosition = getRealPosition();
 
-    bool prevWasOnSlope = m_fields->prevWasOnSlope;
-    m_fields->prevWasOnSlope = m_wasOnSlope;
+    bool wasOnGround = m_fields->wasOnGround;
+    m_fields->wasOnGround = m_isOnGround;
+
 
     if (m_fields->justTeleported) {
       // if we just teleported, we set a streak point to the portal's position and save its location
@@ -114,6 +130,7 @@ class $modify(WTDFPlayerObject, PlayerObject) {
     // with the slope every update
     // this also fires if you just collide with a slope normally
     // but that doesn't mess anything up too badly
+    /*
     if (m_isOnSlope && !m_wasOnSlope && !prevWasOnSlope) {
       CCPoint pointToAdd{nextPosition.x, currentPosition.y};
       addWaveTrailPoint(pointToAdd);
@@ -124,7 +141,7 @@ class $modify(WTDFPlayerObject, PlayerObject) {
       // slope stack
       m_fields->forceAdd = true;
       return;
-    }
+    }*/
 
     cocos2d::CCPoint currentVector = (nextPosition - currentPosition).normalize();
     cocos2d::CCPoint previousVector = (currentPosition - previousPosition).normalize();
@@ -161,18 +178,109 @@ class $modify(WTDFPlayerObject, PlayerObject) {
         std::swap(nextPosition.x, nextPosition.y);
         std::swap(currentPosition.x, currentPosition.y);
       }
-      float intercept = (nextPositionNoCollision.y - currentPosition.y)/(nextPositionNoCollision.x - currentPosition.x);
-      float desiredValue = (nextPosition.y - currentPosition.y + intercept * currentPosition.x)/intercept;
-      // this happens if the player snapped to an object
-      if (
-        (desiredValue < currentPosition.x && !m_isGoingLeft) ||
-        (desiredValue > currentPosition.x && m_isGoingLeft)
-      ) {
-        // draw a vertical if the player snapped
-        if (!m_isOnSlope && !m_wasOnSlope && !prevWasOnSlope) addWaveTrailPoint(currentPosition);
-        desiredValue = currentPosition.x;
+      float objectBoundMinX = std::numeric_limits<float>::lowest();
+      float objectBoundMaxX = std::numeric_limits<float>::max();
+      float objectBoundMinY = std::numeric_limits<float>::lowest();
+      float objectBoundMaxY = std::numeric_limits<float>::max();
+
+      float hitboxSize = getObjectRect().size.width/2;
+
+      float playerConst = currentPosition.x + (m_isGoingLeft ? 2.f : -2.f);
+
+      if (m_fields->collidedSlope) {
+        if (m_isSideways) {
+          objectBoundMinX = m_fields->collidedSlope->getObjectRect().getMinY();
+          objectBoundMaxX = m_fields->collidedSlope->getObjectRect().getMaxY();
+          objectBoundMinY = m_fields->collidedSlope->getObjectRect().getMinX();
+          objectBoundMaxY = m_fields->collidedSlope->getObjectRect().getMaxX();
+        } else {
+          objectBoundMinX = m_fields->collidedSlope->getObjectRect().getMinX();
+          objectBoundMaxX = m_fields->collidedSlope->getObjectRect().getMaxX();
+          objectBoundMinY = m_fields->collidedSlope->getObjectRect().getMinY();
+          objectBoundMaxY = m_fields->collidedSlope->getObjectRect().getMaxY();
+        }
+      } else if (m_collidedObject) {
+        if (m_isSideways) {
+          objectBoundMinX = m_collidedObject->getObjectRect().getMinY();
+          objectBoundMaxX = m_collidedObject->getObjectRect().getMaxY();
+          objectBoundMinY = m_collidedObject->getObjectRect().getMinX();
+          objectBoundMaxY = m_collidedObject->getObjectRect().getMaxX();
+        } else {
+          objectBoundMinX = m_collidedObject->getObjectRect().getMinX();
+          objectBoundMaxX = m_collidedObject->getObjectRect().getMaxX();
+          objectBoundMinY = m_collidedObject->getObjectRect().getMinY();
+          objectBoundMaxY = m_collidedObject->getObjectRect().getMaxY();
+        }
       }
-      CCPoint intersectionPoint{desiredValue, nextPosition.y};
+
+      float intercept = (nextPositionNoCollision.y - currentPosition.y)/(nextPositionNoCollision.x - currentPosition.x);
+      CCPoint intersectionPoint;
+      if (!m_fields->collidedSlope) {
+        intersectionPoint.x = (nextPosition.y - currentPosition.y + intercept * currentPosition.x)/intercept;
+        intersectionPoint.y = nextPosition.y;
+        
+        // this happens if the player snapped to an object
+        if (intersectionPoint.x <= objectBoundMinX - hitboxSize && !m_isGoingLeft) {
+          float newIntersectionY = intercept * (objectBoundMinX - hitboxSize - currentPosition.x) + currentPosition.y;
+          if (m_isSideways)
+            addWaveTrailPoint({newIntersectionY, objectBoundMinX - hitboxSize});
+          else
+            addWaveTrailPoint({objectBoundMinX - hitboxSize, newIntersectionY});
+          
+          intersectionPoint.x = objectBoundMinX - hitboxSize;
+          intersectionPoint.y = nextPosition.y;
+        }
+        else if (intersectionPoint.x >= objectBoundMaxX + hitboxSize && m_isGoingLeft) {
+          float newIntersectionY = intercept * (objectBoundMaxX + hitboxSize - currentPosition.x) + currentPosition.y;
+          if (m_isSideways)
+            addWaveTrailPoint({newIntersectionY, objectBoundMaxX + hitboxSize});
+          else
+            addWaveTrailPoint({objectBoundMaxX + hitboxSize, newIntersectionY});
+          
+          intersectionPoint.x = objectBoundMaxX + hitboxSize;
+          intersectionPoint.y = nextPosition.y;
+        }
+      } else {
+        if (m_isOnGround || wasOnGround) intersectionPoint = currentPosition;
+        else {
+          float slopeAngle = m_unk518;
+          float slopeRotation = m_fields->collidedSlope->getRotation() * M_PI / 180;
+          int slopeFlip = 2 * (m_fields->collidedSlope->isFlipX() == m_fields->collidedSlope->isFlipY()) - 1;
+          float slopeIntercept = slopeFlip * tan(slopeAngle + slopeRotation);
+          intersectionPoint.x = (slopeIntercept*nextPosition.x - intercept*currentPosition.x - nextPosition.y + currentPosition.y)/(slopeIntercept - intercept);
+          intersectionPoint.y = slopeIntercept * (intersectionPoint.x - nextPosition.x) + nextPosition.y;
+          
+          if (
+            (
+              (currentPosition.y - hitboxSize <= objectBoundMinY && hitBottom) ||
+              (currentPosition.y + hitboxSize >= objectBoundMaxY && hitTop)
+            ) && !m_isGoingLeft && !m_wasOnSlope
+          ) {
+            float newIntersectionY = intercept * (objectBoundMinX - hitboxSize - currentPosition.x) + currentPosition.y;
+            if (m_isSideways)
+              addWaveTrailPoint({newIntersectionY, objectBoundMinX - hitboxSize});
+            else
+              addWaveTrailPoint({objectBoundMinX - hitboxSize, newIntersectionY});
+            
+            intersectionPoint.x = objectBoundMinX - hitboxSize;
+            intersectionPoint.y = nextPosition.y;
+          } else if (
+            (
+              (currentPosition.y - hitboxSize <= objectBoundMinY && hitBottom) ||
+              (currentPosition.y + hitboxSize <= objectBoundMaxY && hitTop)
+            ) && m_isGoingLeft && !m_wasOnSlope
+          ) {
+            float newIntersectionY = intercept * (objectBoundMaxX + hitboxSize - currentPosition.x) + currentPosition.y;
+            if (m_isSideways)
+              addWaveTrailPoint({newIntersectionY, objectBoundMaxX + hitboxSize});
+            else
+              addWaveTrailPoint({objectBoundMaxX + hitboxSize, newIntersectionY});
+            
+            intersectionPoint.x = objectBoundMaxX + hitboxSize;
+            intersectionPoint.y = nextPosition.y;
+          }
+        }
+      }
       if (m_isSideways) {
         std::swap(nextPositionNoCollision.x, nextPositionNoCollision.y);
         std::swap(nextPosition.x, nextPosition.y);
@@ -181,13 +289,12 @@ class $modify(WTDFPlayerObject, PlayerObject) {
       }
       addWaveTrailPoint(intersectionPoint);
       m_fields->previousPos = intersectionPoint;
-      m_fields->transitionToCollision = true;
+      m_fields->transitionToCollision = false;
     } else {
       if (nextPosition == nextPositionNoCollision) m_fields->transitionToCollision = false;
       addWaveTrailPoint(currentPosition);
       m_fields->previousPos = currentPosition;
     }
-    
   }
 
   // this will add the requested point to the wave trail, and check if it is (almost) colinear with the last 2 points
